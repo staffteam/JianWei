8f3de8b8968f80c8cc862990634e9c3fe2990d48
wangxd <1144646550@qq.com>
2019-04-01 21:04:48 +0800

    1

---
+++ ContentWeb/js/index.js
@@ -0,0 +1,59 @@
+$(function() {
+	var indexbanner = new Swiper('#indexbanner', {
+		loop: true, // 循环模式选项
+		autoplay: {
+		    delay: 5000,
+		    stopOnLastSlide: false,
+		    disableOnInteraction: true,
+	    },
+		// 如果需要分页器
+		pagination: {
+			el: '.swiper-pagination.a',
+		},
+
+		// 如果需要前进后退按钮
+		navigation: {
+			nextEl: '.swiper-button-next.b',
+			prevEl: '.swiper-button-prev.a',
+		},
+	});
+	//业务领域点击
+	$('#businessDetails ul li').click(function(){
+		var obj = this;
+		if(!$(obj).hasClass('liHover')){
+			$('#businessDetails ul li.txtShow').remove();
+				$('#businessDetails ul li').removeClass('liHover');
+					$(obj).addClass('liHover');
+					var index_ = $(obj).index('#businessDetails ul li')+1;
+					var indexs = index_%4 == 0?4:index_%4;
+					indexs = 4-indexs;
+					var list_ = index_+ indexs-1;
+					if(list_ > $('#businessDetails ul li').length-1){
+						list_ = $('#businessDetails ul li').length-1;
+					}
+					$('#businessDetails ul li').eq(list_).after("<li class='txtShow'><h2>项目简介</h2><div>"+$(this).find('div').html()+"</div><a>查看更多<i class='iconfont' >&#xe636;</i></a></li>");
+				
+		}else{
+			$(obj).removeClass('liHover');
+				$('#businessDetails ul li.txtShow').remove();
+		}
+	});
+	//新闻切换
+	$('.newsNav span').click(function(){
+		$(this).addClass('on').siblings().removeClass('on');
+	})
+	$('.title').each(function(){
+		if($(window).width()>800){
+			$(this).find('div').eq(1).css({'width':'12vw'});
+		}else{
+			$(this).find('div').eq(1).css({'width':'12rem'});
+		}
+	})
+});
+$(window).scroll(function(){
+	$('.title').each(function(){
+		if($('body,html').scrollTop()+$(window).height()>$(this).offset().top){
+			$(this).find('div').eq(1).removeAttr('style');
+		}
+	})
+})
---
+++ ContentWeb/js/public.js
@@ -0,0 +1,131 @@
+$(function() {
+	if($(window).width() >= 800) {
+		var theUA = window.navigator.userAgent.toLowerCase();
+		if((theUA.match(/msie\s\d+/) && theUA.match(/msie\s\d+/)[0]) || (theUA.match(/trident\s?\d+/) && theUA.match(/trident\s?\d+/)[0])) {
+			var ieVersion = theUA.match(/msie\s\d+/)[0].match(/\d+/)[0] || theUA.match(/trident\s?\d+/)[0];
+			if(ieVersion < 9) {
+				alert('您的浏览器版本太低！ 推荐使用:谷歌,火狐,其他双核极速模式如果你的使用的是双核浏览器,请切换到极速模式访问');
+			}
+		}
+
+	}
+	//二级导航初始化
+	publicObj.navtab();
+	/***html默认px初始化***/
+	publicObj.resize();
+	$(window).resize(function() {
+		publicObj.resize();
+	});
+	
+	//logo初始化
+	publicObj.wap()?$('.header .nav>ul>li').eq(3).before('<li class="logo"><a href="/"><img src="../../Contentweb/images/logo.png" /></a></li>'):'';
+	
+	//導航wap
+	    $('.wapbtn').click(function(){
+	    	if($(this).hasClass('on')){
+	    		$(this).removeClass('on');
+	    		$(this).closest('.header').find('.nav').animate({'opacity':'0'},300,function(){
+	    			$(this).hide();
+	    		});
+	    	}else{
+	    		$(this).addClass('on');
+	    		$(this).closest('.header').find('.nav').show().animate({'opacity':'1'},300);
+	    	}
+	    })
+	//点击logo置顶
+	$('#tabnav .logo,.btntop').click(function() {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500)
+	})
+});
+
+var publicObj = {
+	//文档高度
+	getDT: function() {
+		var scrollTop = 0,
+			bodyScrollTop = 0,
+			documentScrollTop = 0;
+		if(document.body) {
+			bodyScrollTop = document.body.scrollTop;
+		}
+		if(document.documentElement) {
+			documentScrollTop = document.documentElement.scrollTop;
+		}
+		scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+		return scrollTop;
+	},
+	//可视窗口高度
+	getWH: function() {
+		var windowHeight = 0;
+		if(document.compatMode == "CSS1Compat") {
+			windowHeight = document.documentElement.clientHeight;
+		} else {
+			windowHeight = document.body.clientHeight;
+		}
+		return windowHeight;
+	},
+	//滚动条滚动高度
+	getSH: function() {
+		var scrollHeight = 0,
+			bodyScrollHeight = 0,
+			documentScrollHeight = 0;
+		if(document.body) {
+			bodyScrollHeight = document.body.scrollHeight;
+		}
+
+		if(document.documentElement) {
+			documentScrollHeight = document.documentElement.scrollHeight;
+		}
+		scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+		return scrollHeight;
+	},
+	resize: function() {
+		document.getElementsByTagName("html")[0].style.fontSize = document.documentElement.clientWidth / 32 + "px";
+	},
+	wap: function() {
+		return $(window).width() > 800;
+	},
+	navtab: function() {
+		//二级导航
+		if(this.wap()) {
+			$('.nav ul li').hover(function() {
+				var len = $(this).find('div ul').length;
+				var h = $(this).find('div ul').eq(0).height() + parseInt($(this).find('div ul li a').eq(0).css('margin-top'));
+				$(this).find('div').height(len * h).css({
+					'opacity': '1',
+					'padding': '0 0'
+				});
+			}, function() {
+				$(this).find('div').height(0).css({
+					'opacity': '0',
+					'padding': '0 0'
+				});
+			})
+		} else {
+			$('.nav ul li').each(function() {
+				$(this).find('div').length > 0 ? $(this).addClass('tab').find('div').before('<i class="iconfont">&#xe604;</i>') : '';
+			})
+			$('.nav ul li.tab>i').click(function() {
+				if(!$(this).hasClass('on')) {
+					$('.nav ul li.tab a i').removeClass('on');
+					$('.nav ul li div').slideUp()
+					$(this).addClass('on');
+					$(this).parent().find('div').slideDown();
+				} else {
+					$(this).removeClass('on');
+					$(this).parent().find('div').slideUp();
+				}
+			})
+		}
+
+	},
+	language: function(obj) {
+		//中英文切换
+		alert(obj);
+	},
+	//退出登录
+	out:function(){
+		location.reload();
+	}
+};
